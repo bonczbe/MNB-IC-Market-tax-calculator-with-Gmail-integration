9abb6f92e9275854c43cca8576454f257d9fb30e
bonczbe <bonczbe@gmail.com>
2026-03-11 15:55:45 +0100

    feat: Add AccountTransaction and YearlyTaxCalculation models with corresponding migrations

---
+++ database/migrations/2026_03_11_143751_create_account_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('account_transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('broker_account_id');
+            $table->date('date');
+            $table->enum('type', ['deposit', 'withdrawal']);
+            $table->decimal('amount', 15, 2);
+            $table->text('note')->nullable();
+            $table->timestamps();
+
+            $table->foreign('broker_account_id')
+                ->references('id')->on('broker_accounts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('account_transactions');
+    }
+};
---
+++ database/migrations/2026_03_11_143824_create_yearly_tax_calculations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('yearly_tax_calculations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('broker_account_id');
+            $table->year('tax_year');
+            $table->decimal('gross_profit', 15, 2);    // bruttó nyereség az évben
+            $table->decimal('loss_carried_forward', 15, 2)->default(0); // felhasznált korábbi veszteség
+            $table->decimal('taxable_income', 15, 2);  // adóalap
+            $table->decimal('tax_amount', 15, 2);       // 15% adó
+            $table->decimal('unused_loss', 15, 2)->default(0);  //fel nem használt veszteség görgetése, fel nem használt összegek öszessége (utolsó 2 éve)
+            $table->timestamps();
+
+            $table->foreign('broker_account_id')
+                ->references('id')->on('broker_accounts');
+            $table->unique(['broker_account_id', 'tax_year']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('yearly_tax_calculations');
+    }
+};

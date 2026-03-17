88494751fa96a35a6fdb514673f98238732da7ad
Bence Boncz <bonczbe@gmail.com>
2026-03-17 18:18:51 +0100

    started to test event fetching, need to work on the holydays too

---
+++ app/Models/ForexEvent.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ForexEvent extends Model
+{
+    protected $fillable = [
+        'date',
+        'name',
+        'previouse',
+        'forecast',
+    ];
+}
---
+++ app/Models/Holyday.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Holyday extends Model
+{
+    protected $fillable = [
+        'date',
+        'name',
+        'status',
+    ];
+}

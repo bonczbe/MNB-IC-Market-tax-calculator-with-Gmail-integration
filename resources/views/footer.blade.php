10cf35647b1ba45c7a504c0d18d8c04992be3ea4
Bence Boncz <bonczbe@gmail.com>
2026-03-21 01:47:21 +0100

    centered the footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,5 @@
-<footer class="">
-    <span class="">© {{ $Year }}
+<footer style="text-align: center;">
+    <span>© {{ $Year }}
         Created by <a href="https://github.com/bonczbe" class="">Bence Boncz</a>. All Rights Reserved.
     </span>
 </footer>

18d5daa3eea7645185b5daad274b677fd14bfb67
bonczbe <bonczbe@gmail.com>
2026-04-26 00:01:21 +0200

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -29,14 +29,13 @@ if (Config::get('schedule.use_mnb_fetcher')) {
 if (Config::get('schedule.use_email_fetcher')) {
     app(Schedule::class)
         ->command('app:email-extract')
-        ->dailyAt('23:59')
-        ->weekdays()
+        ->dailyAt('23:58')
         ->timezone('Europe/Budapest');
 }
 
 if (Config::get('schedule.use_yearly_calculator')) {
     app(Schedule::class)
         ->command('app:calculate-tax-by-account-for-year')
-        ->yearlyOn(12, 31, '23:55')
+        ->yearlyOn(12, 31, '23:59')
         ->timezone('Europe/Budapest');
 }

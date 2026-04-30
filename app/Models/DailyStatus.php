1011cf50325ebe3b814e4746c0c8a5b161a8a70e
Bence Boncz <bonczbe@gmail.com>
2026-04-30 15:57:07 +0200

    feat: implement caching logic on DailyStatus update to enhance performance

--- app/Models/DailyStatus.php
+++ app/Models/DailyStatus.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Cache;
 
 class DailyStatus extends Model
 {
@@ -24,5 +25,14 @@ class DailyStatus extends Model
     public function broker(): BelongsTo
     {
         return $this->belongsTo(BrokerAccount::class, 'broker_account_id');
+    }
+
+    protected static function booted(): void
+    {
+
+        static::updated(function (DailyStatus $dailyStatus) {
+            Cache::forget('DailyStatusCalculated_'.$dailyStatus->id);
+            Cache::forget('weekly_chart_data'.(auth()->user()->id??0).'_'.$dailyStatus->broker->id);
+        });
     }
 }

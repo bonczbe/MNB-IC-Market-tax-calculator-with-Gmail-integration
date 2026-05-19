9bdaaea7a4628b6600a90917cdae7ec7c943327a
Bence Boncz <bonczbe@gmail.com>
2026-05-19 11:56:20 +0000

    fix: update home route redirection to taxCalculator

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ForexEventController;
 use App\Http\Controllers\HolyDayController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => redirect('/admin'))->name('home');
+Route::get('/', fn () => redirect('/taxCalculator'))->name('home');
 
 Route::get('/holydays', [HolyDayController::class, 'getByDate']);
 Route::get('/events', [ForexEventController::class, 'getByDate']);
